6d277859b05295adebc87bda9f56d945483cd6cd
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-03-08 16:52:19 +0200

    Testing Exceptions using assertThrows()

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,5 +1,6 @@
 package com.programming.techie;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,32 @@ class ContactManagerTest {
         contactManager.addContact("Thomas", "Test", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    @Test
+    @DisplayName("Should not create contact when the first name is null")
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Test", "1234567890");
+        });
+    }
+
+    @Test
+    @DisplayName("Should not create contact when the last name is null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Thomas", null, "1234567890");
+        });
+    }
+
+    @Test
+    @DisplayName("Should not create contact when the phone number is null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Thomas", "Test", null);
+        });
     }
 }
