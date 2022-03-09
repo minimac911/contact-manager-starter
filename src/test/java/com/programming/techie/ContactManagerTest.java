9e1fcd66e1550b2b56d80d67bd2f976512b8b97a
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-03-09 14:07:21 +0200

    Added nested tests and maven surefire

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,19 +1,31 @@
 package com.programming.techie;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ContactManagerTest {
 
     private ContactManager contactManager;
@@ -77,6 +89,58 @@ class ContactManagerTest {
         contactManager.addContact("First", "Last", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class ParameterizedTests {
+
+        @DisplayName("Phone Number should start with 0")
+        @ParameterizedTest
+        @ValueSource(strings = {"0123456789", "0123456789", "0123456789"})
+        public void shouldTestPhoneNumberFormat(String phoneNumber) {
+            contactManager.addContact("First", "Last", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("Method source - Phone Number should start with 0")
+        @ParameterizedTest
+        @MethodSource("phoneNumbers")
+        public void shouldTestPhoneNumberFormatWithMethodSource(String phoneNumber) {
+            contactManager.addContact("First", "Last", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        private List<String> phoneNumbers() {
+            return Arrays.asList("0123456789", "0123456789", "0123456789");
+        }
+
+        @DisplayName("CSV source - Phone Number should start with 0")
+        @ParameterizedTest
+        @CsvSource({"0123456789", "0123456789", "0123456789"})
+        public void shouldTestPhoneNumberFormatWithCSVSource(String phoneNumber) {
+            contactManager.addContact("First", "Last", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("CSV file source - Phone Number should start with 0")
+        @ParameterizedTest
+        @CsvFileSource(resources = "/data.csv")
+        public void shouldTestPhoneNumberFormatWithCSVFileSource(String phoneNumber) {
+            contactManager.addContact("First", "Last", phoneNumber);
+            assertFalse(contactManager.getAllContacts().isEmpty());
+            assertEquals(1, contactManager.getAllContacts().size());
+        }
+    }
+
+    @Test
+    @DisplayName("Test should be disabled")
+    @Disabled
+    public void shouldBeDisabled() {
+        throw new RuntimeException("Test should be disabled");
     }
 
     @AfterEach
